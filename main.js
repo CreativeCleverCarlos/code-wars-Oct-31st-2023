8d8321dfa43d56daf6bb1a4ba06f428e618eb51a
Carlos Campbells Laptop <carlos.g.campbell@gmail.com>
2023-10-31 13:49:54 -0400

    Code wars Oct 31st 2023

---
+++ main.js
@@ -0,0 +1,37 @@
+/**
+ * This series of katas will introduce you to basics of doing geometry with computers.
+
+Write the function circleArea/CircleArea which takes in a Circle object and calculates the area of that circle.
+The Circle class can be seen below:
+
+//P.R.E.P
+ 
+ **/
+
+//Parameter = a value will be given, the value will be a number. 
+
+//Return - return to the console the answer
+
+//Example - if a radius of 5 is given, than 79.54 must be notified to the console
+
+function circleArea(radius){
+    // the area of a circle being pi r square
+
+    //everything should fit into one singular line
+
+    console.log(Math.PI*radius**2)
+  }
+
+  circleArea(5)
+
+
+//this was in the example but it just made things more confusing 
+
+//   class Circle {
+//     constructor(center, radius) { 
+//       this.center = center; 
+//       this.radius = radius;
+//     }
+//   }
+
+  
